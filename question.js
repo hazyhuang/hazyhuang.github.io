6052a23a0f30b1f71e77ccb928bba94a4cca16e1
Huang Hua <50547681@qq.com>
2020-01-09 23:22:30 +0800

    up

--- question.js
+++ question.js
@@ -1,9 +1,59 @@
 
+var data = {}
+
+var questions = document.getElementsByClassName("section-container-spacer");
+for(i=0;i<questions.length;i++){
+	console.log(i);
+	
+    var answerInpt = questions[i].getElementsByClassName("answerInpt");
+    var name = answerInpt[0].name;
+	var answer = questions[i].getAttribute('value');
+	data[name] = {answer:answer,choose:"",isCorrect:false}
+	console.log(name);
+	console.log(data[name]);
+}
+
+
 var qest = {Number:1, answer:"A", choose:""};
+var question_group = [];
+
+function getResult(obj){
+	amount = 0;
+	for(i=0;i<questions.length;i++){
+		console.log("序号:"+i);
+		var key = i+1;
+        console.log("关键字:"+data[key+""].answer +" 用户选择："+ data[key+""].choose
+         +"  "+data[key+""].isCorrect);
+		if(data[key+""].isCorrect == false){
+			questions[i].style.display='block';
+
+		}else{
+			questions[i].style.display='none';
+			amount++;
+		}
+		
+	}
+	score = amount/questions.length;
+	score = (score.toFixed(2))*100;
+	var result = document.getElementsByClassName("result");
+	result[0].innerText = "得分："+score+"%";
+	console.log("得分："+score+"%");
+}
 
 function chooseSubmit(obj){
 	console.log(obj.value);
+	console.log(obj.name);
 	obj.checked = "true";
+	var name = obj.name;
+    console.log(data[name].answer);
+    data[name].choose = obj.value;
+    if(data[name].answer == obj.value){
+    	console.log("答对了");
+    	data[name].isCorrect=true;
+    }
+	
+
+
 
 }
 
@@ -17,9 +67,13 @@ function lastQuestion(obj){
 	}
 }
 
-function answer(obj){
+function answerQuestion(obj){
 	console.log('answer');
-	obj.parentElement.children[8].style.display='block';
+	if(obj.parentElement.children[8].style.display == 'block'){
+		obj.parentElement.children[8].style.display='none';
+	}else{
+		obj.parentElement.children[8].style.display='block';
+	}
 
 }
 
